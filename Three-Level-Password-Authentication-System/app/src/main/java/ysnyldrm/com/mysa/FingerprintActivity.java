55c3ad986fda7c36b24f5c202a0bcaff7f7e919d
ysnyldrm0 <ysnyldrm0@hotmail.com>
2018-05-16 15:15:44 +0300

    User Class

--- Three-Level-Password-Authentication-System/app/src/main/java/ysnyldrm/com/mysa/FingerprintActivity.java
+++ Three-Level-Password-Authentication-System/app/src/main/java/ysnyldrm/com/mysa/FingerprintActivity.java
@@ -10,6 +10,5 @@ public class FingerprintActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fingerprint);
 
-        int a ;
     }
 }

977f149a059e2e2c8694b3d1fa46637b1ce8cab9
ysnyldrm0 <ysnyldrm0@hotmail.com>
2018-05-18 17:07:50 +0300

    Database Commit

---
+++ Three-Level-Password-Authentication-System/app/src/main/java/ysnyldrm/com/mysa/User.java
@@ -0,0 +1,20 @@
+package ysnyldrm.com.mysa;
+
+
+
+    public class User {
+        public String id;
+        public String userName;
+        public String email;
+        public String password;
+        public String phoneNumber;
+
+        public User(String id, String userName, String email, String password, String phoneNumber) {
+            this.id = id;
+            this.userName = userName;
+            this.email = email;
+            this.password = password;
+            this.phoneNumber = phoneNumber;
+        }
+
+    }

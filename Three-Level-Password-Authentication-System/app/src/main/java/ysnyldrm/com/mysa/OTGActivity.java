5463807923040dd901e0f646dd3fd462d00aa928
ysnyldrm0 <ysnyldrm0@hotmail.com>
2018-05-20 17:05:15 +0300

    Fingerprint Added !

--- Three-Level-Password-Authentication-System/app/src/main/java/ysnyldrm/com/mysa/OTGActivity.java
+++ Three-Level-Password-Authentication-System/app/src/main/java/ysnyldrm/com/mysa/OTGActivity.java
@@ -3,11 +3,11 @@ package ysnyldrm.com.mysa;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class OtpActivity extends AppCompatActivity {
+public class OTGActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_otp);
+        setContentView(R.layout.activity_otg);
     }
 }
